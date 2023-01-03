de45a1b70f64e62e45e29ed099af7085a735c153
Arif KÜÇÜK <40997102+Junefear@users.noreply.github.com>
2023-01-03 11:45:24 +0300

    second commit

---
+++ pages/character/[slug].js
@@ -0,0 +1,51 @@
+import Layout from '../../components/layout'
+import Head from 'next/head'
+import Link from 'next/Link'
+import slug from 'slug'
+import unfetch from 'isomorphic-unfetch'
+
+function CharacterDetail({ character }) {
+    return (
+        <Layout>
+            <Head>
+                <title>Character Detail</title>
+            </Head>
+            <h1>{character.name}</h1>
+            <figure>
+                <img src={character.image} />
+            </figure>
+
+
+        </Layout>
+    )
+}
+
+export default CharacterDetail
+
+
+export async function getStaticPaths() {
+
+    const data = await unfetch('https://rickandmortyapi.com/api/character/')
+    const character = await data.json();
+
+    return {
+        paths: character.results.map(character => {
+            return { params: { slug: `${slug(character.name)}-${character.id}` } }
+        }),
+        fallback: false
+    }
+
+}
+
+export async function getStaticProps({ params }) {
+    const id = params.slug.split("-").slice(-1)[0]
+
+    //data fetch
+    const data = await unfetch('https://rickandmortyapi.com/api/character/' + id)
+    const character = await data.json();
+    return {
+        props: {
+            character
+        }
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Layout from '../components/layout'
 import Head from 'next/head'
+import Link from 'next/Link'
+import unfetch from 'isomorphic-unfetch'
+import slug from 'slug'
 
-function HomePage() {
+function HomePage({ characters }) {
     return (
         <Layout>
             <Head>
@@ -9,8 +12,30 @@ function HomePage() {
             </Head>
             <h1>Welcome to Next.js!</h1>
 
+            <ul>
+                {characters.results.map(characters => (
+                    <li key={characters.id}>
+                        <Link href="/character/[slug]" as={`/character/${slug(characters.name) }-${characters.id}`}>
+                            {characters.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+
         </Layout>
     )
 }
 
 export default HomePage
+
+export async function getStaticProps() {
+
+    //data fetch
+    const data = await unfetch("https://rickandmortyapi.com/api/character")
+    const characters = await data.json();
+    return {
+        props: {
+            characters
+        }
+    }
+}
